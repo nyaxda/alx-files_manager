ac64a4cf3d5afd867289f24c1a54e32f380b7f41
dan <nyaxda@gmail.com>
2024-09-30 09:54:11 -0500

    refactor: Update Redis client to use ES modules
    The Redis client in the `utils/redis.js` file has been updated to use ES modules instead of CommonJS modules. This allows for better compatibility with modern JavaScript syntax and module systems.

--- utils/redis.js
+++ utils/redis.js
@@ -1,55 +1,63 @@
-const Redis = require('ioredis');
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
 /**
- * Redis client
+ * Represents a Redis client.
  */
 class RedisClient {
   /**
-   * Constructor for RedisClient
+   * Creates a new RedisClient instance.
    */
   constructor() {
-    this.client = new Redis();
-    this.client.on('error', (error) => {
-      console.log(`Redis client not connected to the server: ${error.message}`);
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
     });
   }
 
   /**
-   * Check if client is connected
-   * @returns {boolean} true if client is connected
+   * Checks if this client's connection to the Redis server is active.
+   * @returns {boolean}
    */
   isAlive() {
-    return this.client.status === 'ready';
+    return this.isClientConnected;
   }
 
   /**
-   * Get value from key
-   * @param {String} key
-   * @returns {String} value
+   * Retrieves the value of a given key.
+   * @param {String} key The key of the item to retrieve.
+   * @returns {String | Object}
    */
   async get(key) {
-    const value = await this.client.get(key);
-    return value;
+    return promisify(this.client.GET).bind(this.client)(key);
   }
 
   /**
-   * Set key value pair with duration
-   * @param {String} key
-   * @param {String} value
-   * @param {Number} duration
+   * Stores a key and its value along with an expiration time.
+   * @param {String} key The key of the item to store.
+   * @param {String | Number | Boolean} value The item to store.
+   * @param {Number} duration The expiration time of the item in seconds.
+   * @returns {Promise<void>}
    */
   async set(key, value, duration) {
-    await this.client.set(key, value);
-    await this.client.expire(key, duration);
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
   }
 
   /**
-   * Delete key from redis
-   * @param {String} key
+   * Removes the value of a given key.
+   * @param {String} key The key of the item to remove.
+   * @returns {Promise<void>}
    */
   async del(key) {
-    await this.client.del(key);
+    await promisify(this.client.DEL).bind(this.client)(key);
   }
 }
 
-const redisClient = new RedisClient();
-module.exports = redisClient;
+export const redisClient = new RedisClient();
+export default redisClient;
